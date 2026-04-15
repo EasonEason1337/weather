0da62285b26e017d834347d4079d9b4047cb2b81
brickmonsta <127832950+brickmonsta@users.noreply.github.com>
2026-04-15 17:19:34 +1200

    variables and datatypes

--- javascript/variables.js
+++ javascript/variables.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+const bruh = "bruh"; // use const by defualt
+let admin;
+let name;
+name = "John";
+admin = name;
+console.log(admin); // John
+
+let planet = "Earth";
+let visitor;
+
+typeof undefined; // "undefined"
+
+typeof 0; // "number"
+
+typeof 10n; // "bigint"
+
+typeof true; // "boolean"
+
+typeof "foo"; // "string"
+
+typeof Symbol("id"); // "symbol"
+
+typeof Math; // "object"  (1)
+
+typeof null; // "object"  (2)
+
+typeof alert; // "function"  (3)

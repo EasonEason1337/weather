4cca0fd3a38e5ab54c40cc9ff382439801489194
brickmonsta <127832950+brickmonsta@users.noreply.github.com>
2026-05-11 19:42:27 +1200

    added primitives

---
+++ javascript/primitives.js
@@ -0,0 +1,14 @@
+let john = {
+    name: "John",
+    sayHi: function(){
+        alert("Hi Buddy!")
+    }
+}
+john.sayHi();
+
+let n = 1.23456;
+
+alert( n.toFixed(2) );
+
+let str = "Hello"
+alert(str.toUpperCase());

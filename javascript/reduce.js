7759c359858a6e08d7444b2199c96d5552c339b9
brickmonsta <127832950+brickmonsta@users.noreply.github.com>
2026-05-24 22:27:18 +1200

    pc stuff

---
+++ javascript/reduce.js
@@ -0,0 +1,7 @@
+const regions = [
+  { name: 'Auckland',   population: 1_700_000 },
+  { name: 'Wellington', population: 215_000   },
+  { name: 'Canterbury', population: 510_000   },
+  { name: 'Otago',      population: 230_000   },
+  { name: 'Waikato',    population: 490_000   },
+]

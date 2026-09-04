124c475eb1df035e4e055801b1c064f5f5329f15
brickmonsta <127832950+brickmonsta@users.noreply.github.com>
2026-09-04 14:43:50 +1200

    added labels for wind

--- javascript/utils.js
+++ javascript/utils.js
@@ -1,7 +1,7 @@
 export function formatTemp(temp) {
-  return `${temp}°C`
+  return `${temp}°C`;
 }
 
 export function formatWind(speed, direction) {
-  return `${speed} km/h · ${direction}°`
+  return `Wind speed: ${speed} km/h · Wind direction: ${direction}°`;
 }
